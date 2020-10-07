3e9ef9cd386a1643f5375c775d58a408b282386c
Charlesccharris <harriscc@student.swosu.edu>
2020-10-07 08:57:56 -0500

    Made the linier search, but failed to sort the array

---
+++ Homework7_16.java
@@ -0,0 +1,78 @@
+class Homework7_16{
+	public static void main(String[] args){
+	/*(Execution time)
+	Write a program that randomly generates an array of 100,000 integers and a key.
+	Estimate the execution time of invoking the linearSearch method in Listing 7.6.
+	Sort the array and estimate the execution time of invoking the binarySearch method in Listing 7.7.
+	You can use the following code template to obtain the execution time:
+	long startTime = System.currentTimeMillis();
+	perform the task;
+	long endTime = System.currentTimeMillis();
+	long executionTime = endTime - startTime;*/
+
+//	long startTime = System.currentTimeMillis();
+
+//	int[] bigArray = createArray();
+//	int key = (int)(Math.random()*10000);
+
+	int[] bigArray = {10, 4, 3, 6, 8, 5, 9};
+	int key = 9;
+
+//	int match = linearSearch(bigArray, key);
+//	long endTime = System.currentTimeMillis();
+//	long executeTime = endTime - startTime;
+
+	selectionSort(bigArray);
+
+	int match = linearSearch(bigArray, key);
+
+	System.out.println("The number we are looking for is: " + key);
+	System.out.println(match + " Was the corrisponding square");
+//	System.out.println("It took " + executeTime + " milliseconds, for the linear search to find it.");
+
+	}
+
+	public static int[] createArray(){
+
+		int[] bigArray = new int[100_000];
+
+		for(int count = 0; count < bigArray.length; count++){
+			bigArray[count] = (int)(Math.random()*10000);
+		}
+
+	return bigArray;
+
+	}
+
+
+	public static int linearSearch(int[] bigArray, int key){
+		int count = 0;
+
+
+		for(count = 0; count < bigArray.length; count++){
+			if(key == bigArray[count])
+			return count;
+		}
+
+		return -1;
+	}
+
+	public static void selectionSort(int[] bigArray){
+		for(int count = 0; count < bigArray.length - 1; count++){
+			int currentMin = bigArray[count];
+			int currentMinIndex = count;
+
+			for(int round = count + 1; round < bigArray.length; round++){
+				if(currentMin > bigArray[round]){
+					currentMin = bigArray[count];
+					currentMinIndex = round;
+				}
+			}
+			if(currentMinIndex != count){
+				bigArray[currentMinIndex] = bigArray[count];
+				bigArray[count] = currentMin;
+			}
+			System.out.println("Current min is: " + currentMin);
+		}
+	}
+}
