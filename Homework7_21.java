466a088edba19f1ea7d167320e903e7f91abdf26
Charlesccharris <harriscc@student.swosu.edu>
2020-10-16 11:02:30 -0500

    Changed the formatting, so it was all in-line

--- Homework7_21.java
+++ Homework7_21.java
@@ -2,58 +2,54 @@ import java.util.Scanner;
 
 class Homework7_21{
 	public static void main(String[] args){
-	/*(Game: bean machine)
-	The bean machine, also known as a quincunx or the Galton box,
-	is a device for statistics experiments named after English scientist Sir Francis Galton.
-	It consists of an upright board with evenly spaced nails (or pegs) in a triangular form,
-	Balls are dropped from the opening of the board.
-	Every time a ball hits a nail, it has a 50% chance of falling to the left or to the right.
-	The piles of balls are accumulated in the slots at the bottom of the board.
-	Write a program that simulates the bean machine.
-	Your program should prompt the user to enter the number of the balls and the number of the slots in the machine.
-	Simulate the falling of each ball by printing its path.
-	Display the final buildup of the balls in the slots in a histogram.
-	Here is a sample run of the program:
-	Enter the number of balls to drop: 5;
-	Enter the number of slots in the bean machine: 8;
-	LRLRLRR;
-	RRLLLRR;
-	LLRLLRR;
-	RRLLLLL;
-	LRLRRLR;
-	   O;
-	   O;
-	  OOO
-*/
-	Scanner input = new Scanner(System.in);
-	String finalBoard = "";
-	System.out.print("Enter the number of slots: ");
-	int numberOfSlots = input.nextInt();
+		/*(Game: bean machine)
+		The bean machine, also known as a quincunx or the Galton box,
+		is a device for statistics experiments named after English scientist Sir Francis Galton.
+		It consists of an upright board with evenly spaced nails (or pegs) in a triangular form,
+		Balls are dropped from the opening of the board.
+		Every time a ball hits a nail, it has a 50% chance of falling to the left or to the right.
+		The piles of balls are accumulated in the slots at the bottom of the board.
+		Write a program that simulates the bean machine.
+		Your program should prompt the user to enter the number of the balls and the number of the slots in the machine.
+		Simulate the falling of each ball by printing its path.
+		Display the final buildup of the balls in the slots in a histogram.
+		Here is a sample run of the program:
+		Enter the number of balls to drop: 5;
+		Enter the number of slots in the bean machine: 8;
+		LRLRLRR;
+		RRLLLRR;
+		LLRLLRR;
+		RRLLLLL;
+		LRLRRLR;
+		   O;
+		   O;
+		  OOO*/
 
-	System.out.print("\nEnter the number of balls: ");
-	int numberOfBalls = input.nextInt();
+		Scanner input = new Scanner(System.in);
+		String finalBoard = "";
+		System.out.print("Enter the number of slots: ");
+		int numberOfSlots = input.nextInt();
 
-	int[] nailBoard = new int[numberOfSlots];
+		System.out.print("\nEnter the number of balls: ");
+		int numberOfBalls = input.nextInt();
 
-	fillArray(nailBoard);
+		int[] nailBoard = new int[numberOfSlots];
 
-	hitNail(nailBoard, numberOfBalls);
+		fillArray(nailBoard);
 
-	finalBoard = printArray(nailBoard, finalBoard, numberOfBalls);
+		hitNail(nailBoard, numberOfBalls);
 
-	System.out.print(finalBoard);
+		finalBoard = printArray(nailBoard, finalBoard, numberOfBalls);
+
+		System.out.print(finalBoard);
 	}
 
 	public static void fillArray(int[] array){
-
 		for(int count = 0; count < array.length; count++){
 			array[count] = 0;
 		}
-
 	}
 	public static void hitNail(int[] array, int numberOfBalls){
-
-
 		for(int balls = 0; balls < numberOfBalls; balls++){
 			String nailList = "";
 			int selection = 0;
@@ -87,7 +83,5 @@ class Homework7_21{
                 finalBoard += "\n";
 	}
 	return finalBoard;
-
-
 	}
 }
