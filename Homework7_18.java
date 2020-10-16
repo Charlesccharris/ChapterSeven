b1def26039949fcf7aa1c9592bcb2514e5441c6e
Charlesccharris <harriscc@student.swosu.edu>
2020-10-16 11:00:22 -0500

    Added a bubble sort down

--- Homework7_18.java
+++ Homework7_18.java
@@ -13,14 +13,14 @@ class Homework7_18{
 
 		Scanner input = new Scanner(System.in);
 		double[] list = createList(input);
+		int orderChoice;
 
 		getList(list, input);
-		System.out.print("\nThe original order is: ");
-		displayOrder(list);
 
-		bubbleSort(list);
-		System.out.print("The sorted order is: ");
-		displayOrder(list);
+		orderChoice = getOrder(input);
+
+		sortTheArray(list, orderChoice);
+
 	}
 
 	public static double[] createList(Scanner input){
@@ -32,7 +32,7 @@ class Homework7_18{
 	}
 
 	public static void getList(double[] list, Scanner input){
-	System.out.println("\nYou will need to enter " + list.length + " numbers");
+		System.out.println("\nYou will need to enter " + list.length + " numbers");
 
 		for(int count = 0; count < list.length; count++){
 			System.out.print("Enter number #" + (count + 1) + ": ");
@@ -40,12 +40,44 @@ class Homework7_18{
 		}
 	}
 
-	public static void bubbleSort(double[] list){
-	double temp;
-	boolean outOfOrder = true;
+	public static int getOrder(Scanner input){
+		char choice;
 
-	int rounds = 0;
-	int totalCount = list.length - 1;
+		System.out.print("Do you want to sort it in asxending or descending order: ");
+		choice = input.next().charAt(0);
+		choice = Character.toUpperCase(choice);
+		if(choice == 'A')
+			return 0;
+		else if(choice == 'D');
+			return 1;
+
+	}
+
+	public static void sortTheArray(double[] array, int choice){
+
+		System.out.print("\nThe original order is: ");
+		displayOrder(array);
+
+		switch(choice){
+			case 0:
+				bubbleSortUp(array);
+				System.out.print("\nThe sorted list In ascending order: ");
+				displayOrder(array);
+				break;
+			case 1:
+				bubbleSortDown(array);
+				System.out.print("\nThe sorted list in descending order: ");
+				displayOrder(array);
+				break;
+		}
+	}
+
+	public static void bubbleSortUp(double[] list){
+		double temp;
+		boolean outOfOrder = true;
+
+		int rounds = 0;
+		int totalCount = list.length - 1;
 		while(outOfOrder){
 			outOfOrder = false;
 			for(int count = 0; count < totalCount - rounds; count++){
@@ -53,6 +85,26 @@ class Homework7_18{
 					temp = list[count];
 					list[count] = list[count + 1];
 					list[count + 1] = temp;
+					outOfOrder = true;
+				}
+			}
+			rounds++;
+		}
+	}
+
+	public static void bubbleSortDown(double[] list){
+		double temp;
+		boolean outOfOrder = true;
+
+		int rounds = 0;
+		int totalCount = list.length - 1;
+		while(outOfOrder){
+			outOfOrder = false;
+			for(int count = totalCount; count > 0; count--){
+				if(list[count] > list[count - 1]){
+					temp = list[count];
+					list[count] = list[count - 1];
+					list[count - 1] = temp;
 					outOfOrder = true;
 				}
 			}
